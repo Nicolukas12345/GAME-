76cbb26501fcb067812f327b1edd0cd70eef7170
LuanLuc64 <91232299+LuanLuc64@users.noreply.github.com>
2021-09-29 12:17:31 -0500

    Add files via upload

--- New Unity Project/Assets/Scripts/CameraSystem.cs
+++ New Unity Project/Assets/Scripts/CameraSystem.cs
@@ -8,10 +8,21 @@ public class CameraSystem : MonoBehaviour
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
+    //Follow player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
     private void Update(){
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), 
-        ref velocity, speed * Time.deltaTime);
-    }
+        //Room Camera
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), 
+        //ref velocity, speed * Time.deltaTime);
+
+        //Follow Player
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }   
     public void MoveToNewRoom(Transform _newRoom){
         currentPosX = _newRoom.position.x;
     }
