fc9eac0e6daeece422918b9ac5fc378c0f9b2d00
Nicolukas12345 <80796773+Nicolukas12345@users.noreply.github.com>
2021-09-29 19:41:57 -0500

    Add files via upload

--- New Unity Project/Assets/Scripts/PauseMenu.cs
+++ New Unity Project/Assets/Scripts/PauseMenu.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,4 +21,28 @@ public class Pausemenu : MonoBehaviour
 	}
 
 
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausemenu : MonoBehaviour
+{
+	[SerializeField] GameObject pauseMenu;
+	
+	public void Pause ()
+	{
+		pauseMenu.SetActive(true);
+		Time.timeScale = 2f;
+	}
+	
+	public void Resume()
+	{
+		pauseMenu.SetActive(false);
+		Time.timeScale = 1f;
+	}
+
+
+>>>>>>> 6732494b11c04c4578800bdecd10b86da48108a0
 }
